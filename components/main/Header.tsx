1137cf357584601ed77e2f888c3a35dd1e8004f7
Akshar Barot <akshar.barot67@gmail.com>
2024-01-11 23:37:54 -0500

    fixed webring link

--- components/main/Header.tsx
+++ components/main/Header.tsx
@@ -17,22 +17,24 @@ const Header = () => {
                 >
                     <h2 className="text-lg mt-3 font-medium tracking-tight text-slate-200 sm:text-xl">
                         Software Engineer @ University of Waterloo
+                        <span className="inline-flex">
+                            <svg
+                                className="h-4 w-4 mt-4 ml-2 group-hover:translate-x-1 group-hover:-translate-y-0.5 transition duration-500 ease-in-out"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+                                />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+                                />
+                            </svg>
+                        </span>
                     </h2>
-                    <svg
-                        className="h-4 w-4 mt-4 ml-2 group-hover:translate-x-1 group-hover:-translate-y-0.5 transition duration-500 ease-in-out"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
-                        />
-                        <path
-                            fillRule="evenodd"
-                            d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
-                        />
-                    </svg>
                 </a>
 
                 <p className="mt-4 max-w-xs leading-normal">
